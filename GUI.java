ffcb30cbd77f8277d8a60aa17b1bb4fb53d5e102
Kacper Garncarek <kacpergarncarek@gmail.com>
2022-12-15 19:19:57 +0100

    Added setting answer after clicking the button

--- GUI.java
+++ GUI.java
@@ -2,8 +2,30 @@ package com.sample;
 
 import javax.swing.*;
 import java.awt.GridLayout;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.Enumeration;
 
 class GUI{
+	
+	public static String answer;
+	
+	public String getAnswer() {
+		return answer;
+	}
+	
+	private static String getSelectedButtonText(ButtonGroup buttonGroup) {
+        for (Enumeration<AbstractButton> buttons = buttonGroup.getElements(); buttons.hasMoreElements();) {
+            AbstractButton button = buttons.nextElement();
+
+            if (button.isSelected()) {
+                return button.getText();
+            }
+        }
+
+        return null;
+    }
+	
       public static void window(String tresc_pytania, String[] odpowiedzi,
     		  				    String opcja_domyslna){
 		  JFrame frame = new JFrame(tresc_pytania);
@@ -31,5 +53,23 @@ class GUI{
 		  JButton okButton = new JButton("OK");
 		  frame.getContentPane().add(okButton);
 		  frame.setVisible(true);
+		  
+		  okButton.addMouseListener(new MouseListener() {
+	            @Override
+	            public void mouseClicked(MouseEvent mouseEvent) {
+	                answer = getSelectedButtonText(group);
+	                frame.dispose();
+	            }
+
+	            @Override
+	            public void mousePressed(MouseEvent mouseEvent) {}
+	            @Override
+	            public void mouseReleased(MouseEvent mouseEvent) {}
+	            @Override
+	            public void mouseEntered(MouseEvent mouseEvent) {}
+	            @Override
+	            public void mouseExited(MouseEvent mouseEvent) {}
+	        });
      }
+      
 }
--- GameGenres.java
+++ GameGenres.java
@@ -1,10 +1,9 @@
 package com.sample;
 
+
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
-
-import com.sample.GUI.*;
 
 /**
  * This is a sample class to launch a rule.
@@ -17,9 +16,11 @@ public class GameGenres {
    				 "Mecha", "Generes related to near present",
    				 "Westerns", "Supers", "Other Alternate History",
    				 "Genre Shouldn't matter"};
-        	GUI.window("Which genre interests you?",
-        				answers,
-        				"Medieval Fantasy");
+        	
+    		GUI.window("Which genre interests you?",
+    				answers,
+    				"Medieval Fantasy");
+        	
         	
             // load up the knowledge base
 	        KieServices ks = KieServices.Factory.get();
@@ -27,42 +28,12 @@ public class GameGenres {
         	KieSession kSession = kContainer.newKieSession("ksession-rules");
 
             // go !
-            Message message = new Message();
-            message.setMessage("Hello World");
-            message.setStatus(Message.HELLO);
-            kSession.insert(message);
             kSession.fireAllRules();
         } catch (Throwable t) {
             t.printStackTrace();
         }
     }
 
-    public static class Message {
-
-        public static final int HELLO = 0;
-        public static final int GOODBYE = 1;
-
-        private String message;
-
-        private int status;
-
-        public String getMessage() {
-            return this.message;
-        }
-
-        public void setMessage(String message) {
-            this.message = message;
-        }
-
-        public int getStatus() {
-            return this.status;
-        }
-
-        public void setStatus(int status) {
-            this.status = status;
-        }
-
-    }
     
     
 
